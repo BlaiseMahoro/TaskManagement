d351bf8cfcf325c19ee67ec246d8e94cbe32cbad
Brooke <brookedobbins5@gmail.com>
2020-07-14 20:58:50 -0500

    Initial navigation bar

--- maroon/task_management/urls.py
+++ maroon/task_management/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Redirect.as_view()),
     path('landing', views.Landing.as_view(), name='landing'),
-
+    path('account', views.Account.as_view(), name='account'),
 ]
