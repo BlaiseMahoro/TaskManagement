8886ad115c0fa24c00a2cb0755d15ad42eb1f80e
diegovaz33 <diegovaz33@gmail.com>
2020-07-27 00:09:20 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/B1Dobbs/Maroon-Task-Management""""""
    This reverts commit e4a32b7e7c0436778faed3065827ce8fb92deef2.

--- maroon/maroon/settings.py
+++ maroon/maroon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'task_management.apps.TaskManagementConfig',
     'api.apps.ApiConfig',
+    'bootstrap_modal_forms',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'task_management.project_context.get_projects'
             ],
         },
     },
