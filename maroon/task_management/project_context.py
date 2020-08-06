672f447054dff177b7b10d21dab22dfc40232062
Blaise_Mahoro <blaise.mahoro@eagles.oc.edu>
2020-08-05 19:42:51 -0500

    Merge branch 'master' of https://github.com/B1Dobbs/Maroon-Task-Management

--- maroon/task_management/project_context.py
+++ maroon/task_management/project_context.py
@@ -1,20 +1,18 @@
 from .models import Project, Profile, Role
-from .forms import NewProjectForm
-from django.shortcuts import reverse
 
 #http://kkabardi.me/post/dynamic-menu-navigation-django/
 def get_projects(request):
     admin_projects = []
     normal_projects = []
-    form_redirect = reverse('create_project')
     #print(request.user.is_authenticated)
     if request.user.is_authenticated:
         profile = Profile.objects.get(user=request.user)
         projects = profile.get_user_projects()
         admin_projects = profile.get_user_projects(admin=True)
-        normal_projects = profile.get_user_projects(admin=False)    
+        normal_projects = profile.get_user_projects(admin=False)
+        # print("Admin")
+        # print(admin_projects)
+        # print("Normal")
+        # print(normal_projects)
         
-    return {'admin_projects': admin_projects, 
-        'normal_projects': normal_projects, 
-        'form_redirect': form_redirect,
-        'new_project_form': NewProjectForm()}
+    return {'admin_projects': admin_projects, 'normal_projects': normal_projects}
