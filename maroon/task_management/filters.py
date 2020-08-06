4d2ba0e8ead087586f30c08104b6b80bb7863014
Diego Vazquez <dvazqueb11@gmail.com>
2020-08-06 18:41:12 -0500

    Fix in Backlog

--- maroon/task_management/filters.py
+++ maroon/task_management/filters.py
@@ -1,12 +1,13 @@
 import django_filters
-from django_filters import DateFilter, CharFilter
+from django_filters import DateFilter, CharFilter, ChoiceFilter
 
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
-    created_date = DateFilter(field_name="created_date", lookup_expr='gte')
+    
     Title = CharFilter(field_name="title", lookup_expr='icontains' )
+    Assignees = ChoiceFilter(field_name="assignees")
     class Meta:
         model = Ticket
         fields = '__all__'
-        exclude = ['project', 'id_in_project', 'description', 'updated_date', 'title']
+        exclude = ['project', 'id_in_project', 'description','created_date', 'updated_date', 'title', 'assignees']
